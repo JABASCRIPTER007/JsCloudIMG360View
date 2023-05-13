cfa1d4040b8f6c2eebaea1fac41262646b09102d
ANDRII <105977866+JABASCRIPTER007@users.noreply.github.com>
2023-05-13 21:24:27 +0300

    Update goTo.js

--- localhost/script/js/goTo.js
+++ localhost/script/js/goTo.js
@@ -1,4 +1,32 @@
-// This function adds a click event listener to a specific element with the class 'cloudimage-360-hotspot-custom-icon'.
+// initialize the flag variable
+let isExecuting = false;
+
+//initializing a global variable for the value of the number of frames
+let amount = 0;
+
+//getting the attribute value
+const folderUrl = document.getElementById("gurkha-suv").getAttribute("data-folder");
+
+// Send a request to the server using the folderUrl value
+fetch(folderUrl)
+    // Process the server response as text
+    .then(response => response.text())
+    .then(html => {
+        // Create a new DOMParser object
+        const parser = new DOMParser();
+
+        // Parse the HTML string into a DOM document
+        const htmlDocument = parser.parseFromString(html, "text/html");
+
+        // Find all image links in the DOM document
+        const images = htmlDocument.querySelectorAll('a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"], a[href$=".gif"]');
+
+        // Get the number of images found
+        amount = images.length;
+    })
+    // Handle errors and log them to the console
+    .catch(error => console.error(error));
+
 // If the element is found, the handleClick function is called, and an intervalId is cleared.
 function addClickHandler() {
     const hotspotIcon = document.querySelector('.cloudimage-360-hotspot-custom-icon');
@@ -7,8 +35,16 @@ function addClickHandler() {
         clearInterval(intervalId);
     }
 }
+
 // This function handles the click event on the hotspotIcon element.
 function handleClick(event) {
+    if (isExecuting) {
+        // exit the function if it's already executing
+        return;
+    }
+    // set the flag to true
+    isExecuting = true;
+
     // Get the input element with id 'destSlideInput' and assign it to the variable destSlideInput
     const destSlideInput = document.getElementById('destSlideInput');
 
@@ -20,6 +56,22 @@ function handleClick(event) {
         inputValue = event.target.value;
     });
 
+    // Check if amount is less than the inputValue before executing goTo()
+    if (amount < Number(inputValue)) {
+        alert("Amount must be less than the input value!");
+
+        // reset the flag to false
+        isExecuting = false;
+
+        // exit the function without performing any action
+        return;
+    }else if(Number(inputValue) < 0) {
+        alert("inputValue < 0");
+        // reset the flag to false
+        isExecuting = false;
+        return;
+    }
+
     // This function performs a series of keydown events for the left arrow key based on the value specified in the inputValue variable
     function goTo() {
         var counter = 0;
@@ -28,7 +80,14 @@ function handleClick(event) {
             var event = new KeyboardEvent('keydown', {'keyCode': 37});
             var activeIndex = window.CI360.getActiveIndexByID('gurkha-suv');
 
-            if (activeIndex < Number(inputValue)) {
+            if (activeIndex === Number(inputValue)) {
+
+                // reset the flag to false
+                isExecuting = false;
+
+                // exit the function without performing any action
+                return;
+            } else if (activeIndex < Number(inputValue)) {
                 event = new KeyboardEvent('keydown', {'keyCode': 37});
             } else if (activeIndex > Number(inputValue)) {
                 event = new KeyboardEvent('keydown', {'keyCode': 39});
@@ -38,13 +97,16 @@ function handleClick(event) {
 
             if (activeIndex !== Number(inputValue)) {
                 setTimeout(press, 50);
+            }else {
+                // reset the flag to false
+                isExecuting = false;
             }
         }
 
         press();
     }
-
     goTo();
 }
+
 // This interval checks for the presence of the hotspotIcon element every 1 second and calls the addClickHandler function.
 const intervalId = setInterval(addClickHandler, 1000);
