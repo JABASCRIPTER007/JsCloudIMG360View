d3facb62f3b6cffc1f7615d05352854b18494a69
ANDRII <105977866+JABASCRIPTER007@users.noreply.github.com>
2023-05-20 00:58:45 +0300

    Update hotspots.js

--- localhost/script/js/hotspots.js
+++ localhost/script/js/hotspots.js
@@ -1,142 +1,45 @@
-//assigning elements and converting "posYinput" and "posXinput" variables
+// Getting references to elements and converting variables "posYinput" and "posXinput"
 const posYinput = document.getElementById("posYinput");
 const posXinput = document.getElementById("posXinput");
-const valueY = Number(posYinput.value);
-const valueX = Number(posXinput.value);
-posYinput.addEventListener("input", updateHotspot);
-posXinput.addEventListener("input", updateHotspot);
-
-////assigning elements and converting "fromInput" and "toInput" variables
 const fromInput = document.getElementById("fromInput");
 const toInput = document.getElementById("toInput");
+
+// Adding event listeners to input elements to call the "updateHotspot" function
+posYinput.addEventListener("input", updateHotspot);
+posXinput.addEventListener("input", updateHotspot);
 fromInput.addEventListener("input", updateHotspot);
 toInput.addEventListener("input", updateHotspot);
 
 function updateHotspot() {
-    //converting in Number "posYinput" and "posXinput"
+    // Converting input values to numbers
     const fromValueY = Number(posYinput.value);
     const toValueX = Number(posXinput.value);
-
-    //converting in Number "fromInputValue" and "toInputValue"
     const fromInputValue = Number(fromInput.value);
     const toInputValue = Number(toInput.value);
 
+    // Updating positions in the GURKHA_SUV_HOTSPOTS_CONFIG array
     GURKHA_SUV_HOTSPOTS_CONFIG[0].positions.forEach((position, index) => {
-        //iteration for add new update in "imageIndex"
-        if (index >= fromInputValue - 1 && index <= toInputValue - 1) {
+        if (index >= fromInputValue && index <= toInputValue) {
             position.imageIndex = index + 1;
         } else {
             position.imageIndex = 0;
         }
-        //add new update in "xCoord" and "yCoord"
+
         position.xCoord = toValueX;
         position.yCoord = fromValueY;
     });
-    //method to update the hotspot
+
+    // Adding hotspots to the "gurkha-suv" element using the updated configuration
     window.CI360.addHotspots("gurkha-suv", GURKHA_SUV_HOTSPOTS_CONFIG);
 }
 
+// Configuration object for GURKHA_SUV_HOTSPOTS_CONFIG
 const GURKHA_SUV_HOTSPOTS_CONFIG = [
     {
-        positions: [
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-            { imageIndex: 0, xCoord: valueX, yCoord: valueY },
-        ]
+        positions: Array.from({ length: 200 }, () => ({
+            imageIndex: 0,
+            xCoord: Number(posXinput.value),
+            yCoord: Number(posYinput.value)
+        }))
     }
 ];
