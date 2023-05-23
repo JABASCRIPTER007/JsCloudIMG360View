4731d1291899ebb0f496abe5f2193b1389e20b8d
ANDRII <105977866+JABASCRIPTER007@users.noreply.github.com>
2023-05-23 16:14:07 +0300

    Update checkLengthIMG.js

--- localhost/script/js/checkLengthIMG.js
+++ localhost/script/js/checkLengthIMG.js
@@ -7,7 +7,6 @@ fetch(folderUrl)
     // Process the server response as text
     .then(response => response.text())
     .then(html => {
-
         // Create a new DOMParser object
         const parser = new DOMParser();
 
@@ -18,26 +17,17 @@ fetch(folderUrl)
         const images = htmlDocument.querySelectorAll('a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"], a[href$=".gif"]');
 
         // Get the number of images found
-        amount = images.length - 1;
+        amount = images.length;
 
         // Set the value of the "data-amount-x" attribute to the number of images found
         document.getElementById("gurkha-suv").setAttribute("data-amount-x", amount.toString());
 
-        //assigning a variable to an element
+        // Assigning variables to element IDs
         const uploadForm = document.getElementById("upload-form");
-        const add360View =  document.getElementById("add360View");
+        const add360View = document.getElementById("add360View");
 
-        // Check if the amount of images found is greater than 0
-        if (amount > 0) {
-            // Execute additional code here
-            uploadForm.style.display = "none";
-            add360View.style.display = "block";
-        } else {
-            // Execute alternative code here
-            uploadForm.style.display = "block";
-            add360View.style.display = "none";
-        }
-
+        // Toggle the display style of uploadForm and add360View based on the amount of images found
+        uploadForm.style.display = amount > 0 ? "none" : "block"; // Hide upload form if amount > 0, otherwise show it
+        add360View.style.display = amount > 0 ? "block" : "none"; // Show add360View if amount > 0, otherwise hide it
     })
-    // Handle errors and log them to the console
     .catch(error => console.error(error));
