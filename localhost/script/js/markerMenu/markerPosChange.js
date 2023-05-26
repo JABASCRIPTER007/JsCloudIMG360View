5d70882ce1a22add44f0e9a6a98b9708ca10a748
ANDRII <105977866+JABASCRIPTER007@users.noreply.github.com>
2023-05-26 11:51:52 +0300

    Add files via upload

---
+++ localhost/script/js/markerMenu/markerPosChange.js
@@ -0,0 +1,11 @@
+// Add event listeners to track user input changes
+posYinput.addEventListener('input', handlePosInputChange);
+posXinput.addEventListener('input', handlePosInputChange);
+// Function to handle input changes
+function handlePosInputChange() {
+    // Get the first viewer instance
+    const viewer = window.CI360._viewers[0];
+
+    // Update the marker position with the new values
+    viewer.update();
+}
