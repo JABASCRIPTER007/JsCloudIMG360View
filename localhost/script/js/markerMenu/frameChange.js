012e60572400f82dca02677ec2b1d50ecea58a54
ANDRII <105977866+JABASCRIPTER007@users.noreply.github.com>
2023-05-26 16:43:55 +0300

    Update frameChange.js

--- localhost/script/js/markerMenu/frameChange.js
+++ localhost/script/js/markerMenu/frameChange.js
@@ -11,13 +11,6 @@ function handleInput(event) {
     // Get the input value
     let inputValue = Number(event.target.value);
 
-    // Check if the input value is a valid number
-    if (isNaN(inputValue)) {
-        // Show an alert if the input value is not a number
-        alert("Input value must be a number!");
-        return;
-    }
-
     // Get the first viewer instance
     const viewer = window.CI360._viewers[0];
 
